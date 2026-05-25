ce7b2a0d0786fd7075e37678790f1d8f4d5ba28d
Tekou Patrice <patricetekou341@gmail.com>
2026-05-25 08:59:14 +0100

    Correction : transformation des dossiers YangoCare et YangoDriver en dossiers normaux

---
+++ YangoCare/App.tsx
@@ -0,0 +1,23 @@
+import 'react-native-gesture-handler'; // Obligatoire tout en haut !
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+// Importation des pages depuis le dossier pages
+import ConnexionPage from './pages/connexion';
+import InscriptionPage from './pages/inscription';
+import AccueilPage from './pages/accueil';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Connexion" screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Connexion" component={ConnexionPage} />
+        <Stack.Screen name="Inscription" component={InscriptionPage} />
+        <Stack.Screen name="Accueil" component={AccueilPage} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
